fb38a9f88eabaf1a84f047980be44a811bb40ade
Diemo Heuer <diemo.heuer@altow.de>
2023-01-30 19:48:03 +0100

    Elixir Meetup Update
    Co-authored-by: Noah Leu <NoahLeu@users.noreply.github.com>

--- lib/deck.ex
+++ lib/deck.ex
@@ -3,20 +3,28 @@ defmodule Deck do
   alias Cards.Dream
   alias Cards.Location
 
+  #TODO Liste von Tupeln, Variabler Input
+  #TODO Einfachen GameLoop bauen (Win, Loss, UserInput)
+  #TODO StateMachine Skizzieren
+  #TODO Weitere Funktionen implementieren
+  #TODO Mehr Tests!
+
+  # def add_cards_r(cards, card, count \\ 1)
+  # def add_cards_r(cards, card, 1), do: [card | cards]
+  # def add_cards_r(cards, card, count), do: add_cards_r([card | cards], card, count - 1)
+
   def add_cards(cards, card, count \\ 1) do
     1..count
-    |> Enum.reduce(cards, fn _, cards_acc ->
-      [card | cards_acc]
-    end)
+    |> Enum.reduce(cards, fn _, cards_acc -> [card | cards_acc] end)
   end
 
   def get_default_draw_pile do
     []
     |> add_cards(Dream.get_nightmare(), 10)
-    |> add_cards(Door.get_aquarium_door(), 2)
-    |> add_cards(Door.get_garden_door(), 2)
-    |> add_cards(Door.get_library_door(), 2)
-    |> add_cards(Door.get_observatory_door(), 2)
+    |> add_cards(Door.new(:aquarium), 2)
+    |> add_cards(Door.new(:garden), 2)
+    |> add_cards(Door.new(:library), 2)
+    |> add_cards(Door.new(:observatory), 2)
     |> add_cards(Location.get_aquarium_key(), 3)
     |> add_cards(Location.get_aquarium_moon(), 4)
     |> add_cards(Location.get_aquarium_sun(), 8)
@@ -35,4 +43,7 @@ defmodule Deck do
     cards
     |> Enum.each(&IO.inspect(&1))
   end
+
+
+
 end
