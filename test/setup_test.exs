b978d7c667384162190c861d6ebdd91e3ea2fe9c
DiemoHeuer <70590146+DiemoHeuer@users.noreply.github.com>
2023-03-09 11:40:50 +0100

    repaired setup test

--- test/setup_test.exs
+++ test/setup_test.exs
@@ -1,7 +1,15 @@
 defmodule SetupTest do
   use ExUnit.Case
 
-  test "setup game" do
-    assert Phases.Setup.default_setup().draw_pile |> Enum.count() == 76
+  test "setup game - draw pile" do
+    state = Phases.Setup.default_setup()
+
+    assert state.draw_pile |> Enum.count() == 71
+  end
+
+  test "setup game - personal resources" do
+    state = Phases.Setup.default_setup()
+
+    assert state.personal_resources |> Enum.count() == 5
   end
 end
